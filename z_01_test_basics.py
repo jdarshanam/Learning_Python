cb36e875de348092120cbc4e2b35e5ea02084102
jdarshanam <jagan.darshanam@gmail.com>
2026-06-11 09:32:18 -0500

    more challenges

--- z_01_test_basics.py
+++ z_01_test_basics.py
@@ -65,9 +65,96 @@ except ValueError as ve:
     print(f"ValueError - {ve}")
 
 print("\n\n####### files #########")
-f = open('abc.text',mode='w')
+f = open('../abc.text',mode='w')
 f.write("Initial line")
 f.close()
 
+print("\n\n####### string #########")
+st = "Print only the words that start with s in this Sentence"
+for w in st.split(" ") :
+    if w[0].lower() =='s' :
+        print(w)
+
+for w in st.split(): print(w) if w[0].lower() == 's' else None
+
+print("\n\n####### range #########")
+print(f"even numbers upto 10 {list(range(0,10+1,2))}")
+
+print("\n\n####### string #########")
+def myfunc(*args):
+    return sum(args)
+
+print(myfunc(10,20,30))
+
+def myfunc(*args):
+    return [arg for arg in args if arg % 2 == 0]
+
+print(myfunc(1,2,3,4,5,6))
+
+def myfunc(st):
+    result = []
+    for index, e in enumerate(st):
+        if index % 2 == 0:
+            result.append(e.upper())
+        else:
+            result.append(e.lower())
+    return "".join(result)
+
+print(myfunc("Anthropomorphism"))
+
+def summer_69(nums: list) -> int:
+    total = 0
+    add = True
+    for num in nums:
+        while add:
+            if num != 6:
+                total += num
+                break
+            else:
+                add = False
+                break
+        while not add:
+            if num != 9:
+                break
+            else:
+                add = True
+                break
+    return total
 
 
+print(summer_69([1,2,3,4,5,6,7,8,9,10]))
+
+#solution-I
+def spy_game_007(nums: list) -> bool:
+    only_zeros_seven = []
+    for num in nums:
+        if num in [0,7]:
+            only_zeros_seven.append(num)
+    print(f"only_zeros_seven - {only_zeros_seven}")
+    str_only_zeros_seven = "".join(str(n) for n in only_zeros_seven)
+    print(f"str_only_zeros_seven - {str_only_zeros_seven}")
+    if '007'in str_only_zeros_seven:
+        return True
+    else:
+        return False    
+
+
+#print(spy_game_007([1, 2, 4, 0, 0, 7, 5,1, 2, 4, 0, 0, 7, 5]))  # Returns True
+#print(spy_game_007([1, 0, 2, 4, 0, 5, 7]))  # Returns True
+#print(spy_game_007([1, 7, 2, 0, 4, 5, 0]))  # Returns False
+
+#solution-II
+def spy_game_007_sol_2(nums: list) -> bool:
+    code = [0,0,7]
+    for num in nums:
+        if num == code[0] :
+            code.pop(0)
+    
+        if not code: # code list is empty, means we noticed pattern
+            return True
+        
+    return False
+    
+print(spy_game_007_sol_2([1, 2, 4, 0, 0, 7, 5,1, 2, 4, 0, 0, 7, 5]))  # Returns True
+print(spy_game_007_sol_2([1, 0, 2, 4, 0, 5, 7]))  # Returns True
+print(spy_game_007_sol_2([1, 7, 2, 0, 4, 5, 0]))  # Returns False
