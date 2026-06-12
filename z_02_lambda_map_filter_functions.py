5f6b05145c36c27d1da1830555107e7091497b00
jdarshanam <jagan.darshanam@gmail.com>
2026-06-11 22:32:49 -0500

    lambda functions

--- z_02_lambda_map_filter_functions.py
+++ z_02_lambda_map_filter_functions.py
@@ -32,4 +32,39 @@ nums = [1,2,3,4,5,6]
 print(list(filter(check_even,nums)))
 
 
+## LAMBDA functions
+nums = [1,2,3,4,5,6]
+
+def square(num):
+    return num ** 2
+
+print(list(map(square,nums)))
+
+# the above def square could be converted into
+def square(num): return num ** 2
+
+print(list(map(square,nums)))
+
+#further converted into
+square = lambda num : num ** 2
+
+print(square(5))
+
+print(list(map(square,nums)))
+
+# Now, below is lambda expression 
+# square = lambda num : num ** 2 
+# this could be used in conjuction with map or filter functions.
+squared_nums = list(map(lambda num : num **2, nums)) 
+print(squared_nums)
+
+first_letter_in_names = list(map(lambda name : name[0], names))
+print(first_letter_in_names)
+
+even_nums = list(filter(lambda num : num % 2 == 0, nums))
+print(even_nums)
+ 
+
+
+
 
